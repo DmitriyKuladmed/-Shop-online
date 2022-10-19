a5ccb9848e31d10744b4a0c13de67a48ea902c92
Asus <dima.kulaga5@gmail.com>
2022-10-19 10:57:03 +0300

    created database columns

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
-from sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///shop.db"
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    isActive = db.Column(db.Boolean, nullable=False)
+    placement_date = db.Column(db.DateTime, func.utcnow())
+    item_category = db.Column(db.String, nullable=False)
 
 @app.route('/')
 def index():
